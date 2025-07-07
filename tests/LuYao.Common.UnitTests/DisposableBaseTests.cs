f14819eb75a4553149a2370791a78b7ac66b3377
Soar360 <soar360@live.com>
2025-07-07 22:26:11 +0800

    添加 DisposableBase 类及其单元测试
    实现了一个抽象类 `DisposableBase`，用于提供资源释放的基本功能，包含 `Disposed` 属性、`Dispose` 方法和析构函数。新增的 `DisposableBaseTests` 测试类验证了该类的行为，包括实例化后状态、释放后状态、多次释放的允许性、释放自定义资源以及使用语句的正确性。

---
+++ tests/LuYao.Common.UnitTests/DisposableBaseTests.cs
@@ -0,0 +1,65 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LuYao.Tests;
+
+[TestClass]
+public class DisposableBaseTests
+{
+    private class TestDisposable : DisposableBase
+    {
+        public bool IsDisposed => Disposed;
+        public bool CustomResourceDisposed { get; private set; }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                CustomResourceDisposed = true;
+            }
+            base.Dispose(disposing);
+        }
+    }
+
+    [TestMethod]
+    public void NewInstance_Disposed_ShouldBeFalse()
+    {
+        using var sut = new TestDisposable();
+        Assert.IsFalse(sut.IsDisposed);
+    }
+
+    [TestMethod]
+    public void AfterDispose_Disposed_ShouldBeTrue()
+    {
+        var sut = new TestDisposable();
+        sut.Dispose();
+        Assert.IsTrue(sut.IsDisposed);
+    }
+
+    [TestMethod]
+    public void MultipleDispose_ShouldBeAllowed()
+    {
+        var sut = new TestDisposable();
+        sut.Dispose();
+        sut.Dispose(); // 不应抛出异常
+        Assert.IsTrue(sut.IsDisposed);
+    }
+
+    [TestMethod]
+    public void Dispose_ShouldReleaseCustomResources()
+    {
+        var sut = new TestDisposable();
+        sut.Dispose();
+        Assert.IsTrue(sut.CustomResourceDisposed);
+    }
+
+    [TestMethod]
+    public void UsingStatement_ShouldDisposeCorrectly()
+    {
+        TestDisposable sut;
+        using (sut = new TestDisposable())
+        {
+            Assert.IsFalse(sut.IsDisposed);
+        }
+        Assert.IsTrue(sut.IsDisposed);
+    }
+}
