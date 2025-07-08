9b5a604ced71ab2b4649d5863137d25019b9ad97
Soar360 <Soar360@live.com>
2025-07-08 13:50:04 +0800

    新增 EnumerableExtensions 类及其单元测试
    新增 `EnumerableExtensions` 类，提供对 `IEnumerable<T>` 的扩展方法，包括将集合转换为有序且去重的列表和按指定批次大小分组的功能。同时，新增了 `EnumerableExtensionsTests` 类，对这些扩展方法进行了全面的单元测试，确保其正确性和健壮性。

---
+++ src/LuYao.Common/Collections/EnumerableExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LuYao.Collections;
+
+/// <summary>
+/// 提供对 <see cref="IEnumerable{T}"/> 的扩展方法。
+/// </summary>
+public static class EnumerableExtensions
+{
+    /// <summary>
+    /// 使用指定的比较器，将 <see cref="IEnumerable{T}"/> 集合转换为有序且去重的 <see cref="List{T}"/>。
+    /// </summary>
+    /// <typeparam name="T">集合中元素的类型。</typeparam>
+    /// <param name="items">要转换的 <see cref="IEnumerable{T}"/> 集合。</param>
+    /// <param name="comparer">用于元素排序的比较器。</param>
+    /// <returns>包含原集合元素的有序且去重的 <see cref="List{T}"/>。</returns>
+    public static List<T> ToSortedDistinctList<T>(IEnumerable<T> items, IComparer<T> comparer)
+    {
+        var list = new List<T>();
+        foreach (var value in items)
+        {
+            var i = list.BinarySearch(value, comparer);
+            if (i < 0) list.Insert(-i - 1, value);
+        }
+        return list;
+    }
+
+    /// <summary>
+    /// 使用默认比较器，将 <see cref="IEnumerable{T}"/> 集合转换为有序且去重的 <see cref="List{T}"/>。
+    /// </summary>
+    /// <typeparam name="T">集合中元素的类型。</typeparam>
+    /// <param name="items">要转换的 <see cref="IEnumerable{T}"/> 集合。</param>
+    /// <returns>包含原集合元素的有序且去重的 <see cref="List{T}"/>。</returns>
+    public static List<T> ToSortedDistinctList<T>(IEnumerable<T> items) => ToSortedDistinctList(items, Comparer<T>.Default);
+
+    /// <summary>
+    /// 将一个可枚举对象按指定批次大小分组。
+    /// </summary>
+    /// <typeparam name="T">元素类型。</typeparam>
+    /// <param name="source">要分组的源可枚举对象。</param>
+    /// <param name="batchSize">每个批次的元素数量。</param>
+    /// <returns>包含元素列表的批次的可枚举对象。</returns>
+    public static IEnumerable<IReadOnlyList<T>> SplitToBatch<T>(this IEnumerable<T> source, int batchSize)
+    {
+        if (source == null) throw new ArgumentNullException(nameof(source));
+        if (batchSize <= 0) throw new ArgumentOutOfRangeException(nameof(batchSize));
+
+        var buffer = new List<T>(batchSize);
+
+        foreach (var item in source)
+        {
+            buffer.Add(item);
+            if (buffer.Count >= batchSize)
+            {
+                yield return buffer.ToArray();
+                buffer.Clear();
+            }
+        }
+
+        if (buffer.Count > 0)
+        {
+            yield return buffer.ToArray();
+            buffer.Clear();
+        }
+    }
+
+}
---
+++ tests/LuYao.Common.UnitTests/Collections/EnumerableExtensionsTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuYao.Collections;
+
+[TestClass]
+public class EnumerableExtensionsTests
+{
+    [TestMethod]
+    public void ToSortedDistinctList_IntListWithDuplicates_ReturnsSortedDistinctList()
+    {
+        var input = new List<int> { 3, 1, 2, 3, 2, 1 };
+        var result = EnumerableExtensions.ToSortedDistinctList(input);
+
+        CollectionAssert.AreEqual(new List<int> { 1, 2, 3 }, result);
+    }
+
+    [TestMethod]
+    public void ToSortedDistinctList_EmptyList_ReturnsEmptyList()
+    {
+        var input = new List<int>();
+        var result = EnumerableExtensions.ToSortedDistinctList(input);
+
+        Assert.AreEqual(0, result.Count);
+    }
+
+    [TestMethod]
+    public void ToSortedDistinctList_CustomComparer_SortsAccordingToComparer()
+    {
+        var input = new List<string> { "a", "B", "b", "A" };
+        var comparer = StringComparer.OrdinalIgnoreCase;
+        var result = EnumerableExtensions.ToSortedDistinctList(input, comparer);
+
+        CollectionAssert.AreEqual(new List<string> { "a", "B" }, result);
+    }
+
+    [TestMethod]
+    public void SplitToBatch_ListWithExactBatches_ReturnsCorrectBatches()
+    {
+        var input = Enumerable.Range(1, 6).ToList();
+        var batches = input.SplitToBatch(2).ToList();
+
+        Assert.AreEqual(3, batches.Count);
+        CollectionAssert.AreEqual(new List<int> { 1, 2 }, batches[0].ToList());
+        CollectionAssert.AreEqual(new List<int> { 3, 4 }, batches[1].ToList());
+        CollectionAssert.AreEqual(new List<int> { 5, 6 }, batches[2].ToList());
+    }
+
+    [TestMethod]
+    public void SplitToBatch_ListWithRemainder_ReturnsLastBatchWithFewerItems()
+    {
+        var input = Enumerable.Range(1, 5).ToList();
+        var batches = input.SplitToBatch(2).ToList();
+
+        Assert.AreEqual(3, batches.Count);
+        CollectionAssert.AreEqual(new List<int> { 1, 2 }, batches[0].ToList());
+        CollectionAssert.AreEqual(new List<int> { 3, 4 }, batches[1].ToList());
+        CollectionAssert.AreEqual(new List<int> { 5 }, batches[2].ToList());
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void SplitToBatch_NullSource_ThrowsArgumentNullException()
+    {
+        List<int> input = null;
+        _ = input.SplitToBatch(2).ToList();
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void SplitToBatch_BatchSizeZero_ThrowsArgumentOutOfRangeException()
+    {
+        var input = Enumerable.Range(1, 3);
+        _ = input.SplitToBatch(0).ToList();
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void SplitToBatch_BatchSizeNegative_ThrowsArgumentOutOfRangeException()
+    {
+        var input = Enumerable.Range(1, 3);
+        _ = input.SplitToBatch(-1).ToList();
+    }
+}
