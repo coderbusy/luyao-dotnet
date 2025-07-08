fc79db1e7361305de419eabc7823c621447bbe74
Soar360 <Soar360@live.com>
2025-07-08 12:05:44 +0800

    添加 NuGet 发布支持和工作流配置
    在 `build.schema.json` 中新增 `NugetApiKey` 和 `Solution` 字段，支持打包和发布操作。
    在 `Build.cs` 中定义相关参数，并实现 `Compile`、`Test`、`Pack` 和 `Publish` 目标的逻辑，确保使用 NuGet API 密钥推送包。
    新增 `nuget-publish.yml` 文件，配置 GitHub Actions 工作流以自动发布 NuGet 包。

--- build/Build.cs
+++ build/Build.cs
@@ -23,31 +23,66 @@ class Build : NukeBuild
 
     public static int Main() => Execute<Build>(x => x.Compile);
 
+    [Parameter] readonly string NugetApiKey;
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    [Solution] readonly Solution Solution;
+    AbsolutePath OutputDirectory => RootDirectory / "output";
 
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            OutputDirectory.CreateOrCleanDirectory();
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(s => s.SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
         });
 
     Target Test => _ => _
-    .Executes(() =>
-    {
-        DotNetTest(s => s
-            .SetProjectFile("LuYao.sln")
-            .SetConfiguration(Configuration.Debug));
-    });
+        .Executes(() =>
+        {
+            DotNetTest(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration.Debug));
+        });
+
+    Target Pack => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetPack(s => s
+                .SetProject(Solution)
+                .SetConfiguration(Configuration)
+                .SetOutputDirectory(OutputDirectory)
+                .EnableNoBuild());
+        });
+
+    Target Publish => _ => _
+        .DependsOn(Pack)
+        .Requires(() => NugetApiKey)
+        .Executes(() =>
+        {
+            var packageFiles = OutputDirectory.GlobFiles("*.nupkg");
+            foreach (var pkg in packageFiles)
+            {
+                DotNetNuGetPush(s => s
+                    .SetTargetPath(pkg)
+                    .SetSource("https://api.nuget.org/v3/index.json")
+                    .SetApiKey(NugetApiKey));
+            }
+        });
 }
