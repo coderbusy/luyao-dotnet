396f687d642267b742843a9b09406ba9a4b2aa73
Soar360 <Soar360@live.com>
2025-07-14 17:39:54 +0800

    新增日志接口及实现多个日志记录器
    添加了 `ILogger` 接口，提供 `Trace`、`Message`、`Warning`、`Error` 和 `Fatal` 方法，并支持调用者成员名称的自动传递。引入 `LogContext` 结构体，包含日志信息并实现比较和哈希功能。定义 `LogLevel` 枚举以表示不同的日志等级。
    
    实现了多个日志记录器类，如 `ActionLogger`、`AsyncConsoleLogger`、`ConsoleLogger` 和 `TextFileLogger`，支持同步和异步日志记录。`CompositeLogger` 类允许组合多个日志记录器，支持同时向多个目标输出日志。
    
    `TextFileLogger` 提供日志文件管理功能，包括行数和文件大小限制，以及日志覆盖选项。添加 `TextFileLoggerExtensions` 扩展方法，提供额外功能以检查最大文件大小和行数限制。所有日志记录器均实现 `ILogger` 接口，确保一致性。

---
+++ src/LuYao.Common/Logging/Loggers/NullLogger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace LuYao.Logging.Loggers;
+
+/// <summary>
+/// 一个空实现的日志记录器，所有方法均未实现。
+/// </summary>
+public sealed class NullLogger : ILogger
+{
+    /// <summary>  
+    /// 获取 NullLogger 的单例实例。  
+    /// </summary>  
+    public static NullLogger Instance { get; } = new NullLogger();
+
+    /// <inheritdoc />
+    public void Error(string message, Exception? exception = null, [CallerMemberName] string? callerMemberName = null)
+    {
+    }
+
+    /// <inheritdoc />
+    public void Error(Exception exception, string? message = null, [CallerMemberName] string? callerMemberName = null)
+    {
+    }
+
+    /// <inheritdoc />
+    public void Fatal(Exception exception, string message, [CallerMemberName] string? callerMemberName = null)
+    {
+    }
+
+    /// <inheritdoc />
+    public void Message(string text, [CallerMemberName] string? callerMemberName = null)
+    {
+    }
+
+    /// <inheritdoc />
+    public void Trace(string text, [CallerMemberName] string? callerMemberName = null)
+    {
+    }
+
+    /// <inheritdoc />
+    public void Warning(string message, [CallerMemberName] string? callerMemberName = null)
+    {
+    }
+}
