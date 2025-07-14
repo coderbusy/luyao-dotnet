b14d8d7334468e9b42b6456c2397ebb2478f5667
Soar360 <Soar360@live.com>
2025-07-14 15:24:12 +0800

    新增 IPAddressExtensions 类及其测试
    新增 `IPAddressExtensions` 类及其扩展方法 `IsPrivateIPAddress`，用于判断给定的 IP 地址是否为私有地址，支持 IPv4 和 IPv6。新增 `IPAddressExtensionsTests` 测试类，包含多个单元测试，验证该方法在不同场景下的正确性。

---
+++ tests/LuYao.Common.UnitTests/Net/IPAddressExtensionsTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+
+namespace LuYao.Net;
+
+[TestClass]
+public class IPAddressExtensionsTests
+{
+    [TestMethod]
+    public void IsPrivateIPAddress_LoopbackAddress_ReturnsTrue()
+    {
+        var ip = IPAddress.Loopback;
+        Assert.IsTrue(ip.IsPrivateIPAddress());
+    }
+
+    [TestMethod]
+    public void IsPrivateIPAddress_IPv4MappedToIPv6_ReturnsTrue()
+    {
+        var ip = IPAddress.Parse("::ffff:192.168.1.1");
+        Assert.IsTrue(ip.IsPrivateIPAddress());
+    }
+
+    [TestMethod]
+    public void IsPrivateIPAddress_PrivateIPv4Address_ReturnsTrue()
+    {
+        var ip = IPAddress.Parse("192.168.1.1");
+        Assert.IsTrue(ip.IsPrivateIPAddress());
+    }
+
+    [TestMethod]
+    public void IsPrivateIPAddress_PublicIPv4Address_ReturnsFalse()
+    {
+        var ip = IPAddress.Parse("8.8.8.8");
+        Assert.IsFalse(ip.IsPrivateIPAddress());
+    }
+
+    [TestMethod]
+    public void IsPrivateIPAddress_PrivateIPv6Address_ReturnsTrue()
+    {
+        var ip = IPAddress.Parse("fd00::1");
+        Assert.IsTrue(ip.IsPrivateIPAddress());
+    }
+
+    [TestMethod]
+    public void IsPrivateIPAddress_PublicIPv6Address_ReturnsFalse()
+    {
+        var ip = IPAddress.Parse("2001:4860:4860::8888");
+        Assert.IsFalse(ip.IsPrivateIPAddress());
+    }
+
+    [TestMethod]
+    public void IsPrivateIPAddress_IPv6LinkLocalAddress_ReturnsTrue()
+    {
+        var ip = IPAddress.Parse("fe80::1");
+        Assert.IsTrue(ip.IsPrivateIPAddress());
+    }
+
+    [TestMethod]
+    public void IsPrivateIPAddress_IPv6MulticastAddress_ReturnsTrue()
+    {
+        var ip = IPAddress.Parse("ff02::1");
+        Assert.IsTrue(ip.IsPrivateIPAddress());
+    }
+}
