a4d65969e4f4d7a53c8241ab8cb229df1bd88361
Soar360 <Soar360@live.com>
2025-07-14 17:51:49 +0800

    新增更新操作相关类和枚举
    新增了多个类和枚举以支持更新操作，包括 `BuildParameters`、`UpdateFilePackage`、`UpdateFilePackageCollection`、`UpdatePackage` 和 `UpdateStatus`。这些类提供了更新所需的参数和状态信息，如源路径、目标路径、版本号和文件包信息。
    
    `BuildParameters` 类中添加了配置更新操作的属性，`UpdateFilePackage` 类表示单个更新文件包，`UpdateFilePackageCollection` 用于管理多个文件包。`UpdatePackage` 类包含更新包的描述、构建时间和文件包集合，并提供检查更新的方法。`UpdatePackageHelper` 类提供计算文件哈希值的静态方法，确保文件完整性。`UpdateStatus` 枚举定义了更新过程中的不同状态。

---
+++ src/LuYao.Common/IO/Updating/UpdateFilePackageCollection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LuYao.IO.Updating;
+
+/// <summary>  
+/// 表示更新文件包集合。  
+/// </summary>  
+[Serializable]
+public class UpdateFilePackageCollection : List<UpdateFilePackage> { }
---
+++ src/LuYao.Common/IO/Updating/UpdateStatus.cs
@@ -0,0 +1,27 @@
+﻿namespace LuYao.IO.Updating;
+
+/// <summary>
+/// 表示更新状态的枚举类型。
+/// </summary>
+public enum UpdateStatus
+{
+    /// <summary>
+    /// 正在检查更新。
+    /// </summary>
+    Checking,
+
+    /// <summary>
+    /// 正在更新。
+    /// </summary>
+    Updating,
+
+    /// <summary>
+    /// 更新成功。
+    /// </summary>
+    Success,
+
+    /// <summary>
+    /// 更新失败。
+    /// </summary>
+    Fail
+}
