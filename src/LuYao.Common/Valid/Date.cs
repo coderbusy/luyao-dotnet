70c244686b043ddfcb89ae0da8daefc0a47476fd
Soar360 <Soar360@live.com>
2025-07-21 16:06:16 +0800

    新增日期格式选项并更新格式化方法
    在 `Date.cs` 文件中，`Date` 枚举添加了多个新的日期格式选项，包括“月日”、“小时分钟秒”、“小时分钟”、“年月日小时分钟”、“月日年小时分钟”和“月日小时分钟”。原有的“全部显示”选项被保留。
    
    在 `Valid.String.cs` 文件中，`ToString` 方法的 `switch` 语句中添加了对新格式的支持，以便根据不同的 `Date` 枚举值格式化 `DateTime` 对象，新增了对上述新格式的处理逻辑。

--- src/LuYao.Common/Valid/Date.cs
+++ src/LuYao.Common/Valid/Date.cs
@@ -10,7 +10,31 @@ public enum Date
     /// </summary>
     年月日,
     /// <summary>
+    /// 月日
+    /// </summary>
+    月日,
+    /// <summary>
+    /// 小时分钟秒
+    /// </summary>
+    小时分钟秒,
+    /// <summary>
+    /// 小时分钟
+    /// </summary>
+    小时分钟,
+    /// <summary>
+    /// 年月日小时分钟
+    /// </summary>
+    年月日小时分钟,
+    /// <summary>
+    /// 月日年小时分钟
+    /// </summary>
+    月日年小时分钟,
+    /// <summary>
+    /// 月日小时分钟
+    /// </summary>
+    月日小时分钟,
+    /// <summary>
     /// 年月日时分秒全部显示
     /// </summary>
-    全部显示
+    全部显示,
 }
--- src/LuYao.Common/Valid/Valid.String.cs
+++ src/LuYao.Common/Valid/Valid.String.cs
@@ -149,6 +149,12 @@ partial class Valid
         if (dt == default(DateTime)) return string.Empty;
         switch (format)
         {
+            case Date.月日: return dt.ToString("MM/dd", DateTimeFormatInfo.InvariantInfo);
+            case Date.小时分钟秒: return dt.ToString("HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
+            case Date.小时分钟: return dt.ToString("HH:mm", DateTimeFormatInfo.InvariantInfo);
+            case Date.年月日小时分钟: return dt.ToString("yyyy/MM/dd HH:mm", DateTimeFormatInfo.InvariantInfo);
+            case Date.月日年小时分钟: return dt.ToString("MM/dd/yyyy HH:mm", DateTimeFormatInfo.InvariantInfo);
+            case Date.月日小时分钟: return dt.ToString("MM/dd HH:mm", DateTimeFormatInfo.InvariantInfo);
             case Date.全部显示: return dt.ToString("yyyy/MM/dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
             default: return dt.ToString("yyyy/MM/dd", DateTimeFormatInfo.InvariantInfo);
         }
