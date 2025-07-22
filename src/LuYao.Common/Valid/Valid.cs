25fb93e0bb0ed2cbfa4b3f5a2515cc41317583c0
Soar360 <Soar360@live.com>
2025-07-22 16:55:46 +0800

    实现数据类型转换方法
    在 `Valid.cs` 文件中，添加了使用 `Convert` 类的多种数据类型转换方法。删除了 `Valid.NotImplemented.cs` 文件中未实现的相应方法。这些更改使得 `Valid` 类能够实际执行类型转换，提升了代码的可用性。

--- src/LuYao.Common/Valid/Valid.cs
+++ src/LuYao.Common/Valid/Valid.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LuYao;
 
@@ -11,4 +7,39 @@ namespace LuYao;
 /// </summary>
 public static partial class Valid
 {
+    /// <inheritdoc/>
+    public static Byte ToByte(object value) => Convert.ToByte(value);
+
+    /// <inheritdoc/>
+    public static Char ToChar(object value) => Convert.ToChar(value);
+
+    /// <inheritdoc/>
+    public static DateTime ToDateTime(object value) => Convert.ToDateTime(value);
+
+    /// <inheritdoc/>
+    public static Decimal ToDecimal(object value) => Convert.ToDecimal(value);
+
+    /// <inheritdoc/>
+    public static Double ToDouble(object value) => Convert.ToDouble(value);
+
+    /// <inheritdoc/>
+    public static Int16 ToInt16(object value) => Convert.ToInt16(value);
+
+    /// <inheritdoc/>
+    public static Int64 ToInt64(object value) => Convert.ToInt64(value);
+
+    /// <inheritdoc/>
+    public static SByte ToSByte(object value) => Convert.ToSByte(value);
+
+    /// <inheritdoc/>
+    public static Single ToSingle(object value) => Convert.ToSingle(value);
+
+    /// <inheritdoc/>
+    public static UInt16 ToUInt16(object value) => Convert.ToUInt16(value);
+
+    /// <inheritdoc/>
+    public static UInt32 ToUInt32(object value) => Convert.ToUInt32(value);
+
+    /// <inheritdoc/>
+    public static UInt64 ToUInt64(object value) => Convert.ToUInt64(value);
 }
