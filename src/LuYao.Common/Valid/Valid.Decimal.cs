d4306e7a3fc89465f6c8d74055188baee3463881
Soar360 <soar360@live.com>
2025-07-24 22:08:23 +0800

    补充空文件

---
+++ src/LuYao.Common/Valid/Valid.Decimal.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LuYao;
+
+partial class Valid
+{
+
+}
--- src/LuYao.Common/Valid/Valid.cs
+++ src/LuYao.Common/Valid/Valid.cs
@@ -4,6 +4,7 @@ namespace LuYao;
 
 /// <summary>
 /// 数据类型转换
+/// 尽量转换，如果无法完成转换则返回默认值。尽量不抛出异常。
 /// </summary>
 public static partial class Valid
 {
