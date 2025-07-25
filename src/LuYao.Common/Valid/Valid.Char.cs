ced5c5688a70eff6e60de86487eef52374a60438
Soar360 <Soar360@live.com>
2025-07-25 17:31:32 +0800

    优化 Char 类型转换方法实现
    在 `Valid.Char.cs` 文件中，多个静态方法的实现进行了修改。原先使用 `Convert` 类的方法进行类型转换，现在改为直接进行类型转换，并在超出范围时返回默认值。这提高了类型转换的安全性和灵活性。

--- src/LuYao.Common/Valid/Valid.Char.cs
+++ src/LuYao.Common/Valid/Valid.Char.cs
@@ -5,33 +5,47 @@ namespace LuYao;
 partial class Valid
 {
     /// <inheritdoc/>
-    public static Boolean ToBoolean(Char value) => false;
+    public static Boolean ToBoolean(Char value) => default;
+
     /// <inheritdoc/>
     public static Char ToChar(Char value) => value;
+
     /// <inheritdoc/>
-    public static SByte ToSByte(Char value) => Convert.ToSByte(value);
+    public static SByte ToSByte(Char value) => value <= SByte.MaxValue ? (SByte)value : default;
+
     /// <inheritdoc/>
-    public static Byte ToByte(Char value) => Convert.ToByte(value);
+    public static Byte ToByte(Char value) => value <= Byte.MaxValue ? (Byte)value : default;
+
     /// <inheritdoc/>
-    public static Int16 ToInt16(Char value) => Convert.ToInt16(value);
+    public static Int16 ToInt16(Char value) => value <= Int16.MaxValue ? (Int16)value : default;
+
     /// <inheritdoc/>
-    public static UInt16 ToUInt16(Char value) => Convert.ToUInt16(value);
+    public static UInt16 ToUInt16(Char value) => (UInt16)value;
+
     /// <inheritdoc/>
-    public static Int32 ToInt32(Char value) => Convert.ToInt32(value);
+    public static Int32 ToInt32(Char value) => (Int32)value;
+
     /// <inheritdoc/>
-    public static UInt32 ToUInt32(Char value) => Convert.ToUInt32(value);
+    public static UInt32 ToUInt32(Char value) => (UInt32)value;
+
     /// <inheritdoc/>
-    public static Int64 ToInt64(Char value) => Convert.ToInt64(value);
+    public static Int64 ToInt64(Char value) => (Int64)value;
+
     /// <inheritdoc/>
-    public static UInt64 ToUInt64(Char value) => Convert.ToUInt64(value);
+    public static UInt64 ToUInt64(Char value) => (UInt64)value;
+
     /// <inheritdoc/>
-    public static Single ToSingle(Char value) => Convert.ToSingle(value);
+    public static Single ToSingle(Char value) => (Single)value;
+
     /// <inheritdoc/>
-    public static Double ToDouble(Char value) => Convert.ToDouble(value);
+    public static Double ToDouble(Char value) => (Double)value;
+
     /// <inheritdoc/>
-    public static Decimal ToDecimal(Char value) => Convert.ToDecimal(value);
+    public static Decimal ToDecimal(Char value) => (Decimal)value;
+
     /// <inheritdoc/>
-    public static DateTime ToDateTime(Char value) => Convert.ToDateTime(value);
+    public static DateTime ToDateTime(Char value) => default;
+
     /// <inheritdoc/>
-    public static String ToString(Char value) => Convert.ToString(value);
+    public static String ToString(Char value) => value.ToString();
 }
