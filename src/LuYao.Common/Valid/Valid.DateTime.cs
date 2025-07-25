e14d45e92830fda04f3072077bdac56d889627d0
Soar360 <Soar360@live.com>
2025-07-25 15:50:14 +0800

    更新 Valid.DateTime.cs 中的 ToDateTime 方法
    - 添加了 `using System.Globalization;` 语句。
    - 将多个 `ToDateTime` 方法的参数类型从大写更改为小写。
    - 所有 `ToDateTime` 方法的实现改为返回 `default`。
    - 为 `int`, `uint`, `ulong`, `float`, `double`, 和 `decimal` 类型的 `ToDateTime` 方法添加了范围检查。
    - 为 `ulong` 类型的 `ToDateTime` 方法添加了超出范围的注释。
    - 保持所有方法的文档注释不变。

--- src/LuYao.Common/Valid/Valid.DateTime.cs
+++ src/LuYao.Common/Valid/Valid.DateTime.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace LuYao;
 
@@ -16,29 +17,74 @@ partial class Valid
     }
 
     /// <inheritdoc/>
-    public static DateTime ToDateTime(Boolean value) => Convert.ToDateTime(value);
+    public static DateTime ToDateTime(bool value) => default;
+
     /// <inheritdoc/>
-    public static DateTime ToDateTime(Char value) => Convert.ToDateTime(value);
+    public static DateTime ToDateTime(char value) => default;
+
     /// <inheritdoc/>
-    public static DateTime ToDateTime(SByte value) => Convert.ToDateTime(value);
+    public static DateTime ToDateTime(sbyte value) => default;
+
     /// <inheritdoc/>
-    public static DateTime ToDateTime(Byte value) => Convert.ToDateTime(value);
+    public static DateTime ToDateTime(byte value) => default;
+
     /// <inheritdoc/>
-    public static DateTime ToDateTime(Int16 value) => Convert.ToDateTime(value);
+    public static DateTime ToDateTime(short value) => default;
+
     /// <inheritdoc/>
-    public static DateTime ToDateTime(UInt16 value) => Convert.ToDateTime(value);
+    public static DateTime ToDateTime(ushort value) => default;
+
     /// <inheritdoc/>
-    public static DateTime ToDateTime(Int32 value) => Convert.ToDateTime(value);
+    public static DateTime ToDateTime(int value)
+    {
+        long ticks = value;
+        if (ticks < DateTime.MinValue.Ticks || ticks > DateTime.MaxValue.Ticks) return default;
+        return new DateTime(ticks);
+    }
+
     /// <inheritdoc/>
-    public static DateTime ToDateTime(UInt32 value) => Convert.ToDateTime(value);
+    public static DateTime ToDateTime(uint value)
+    {
+        long ticks = value;
+        if (ticks < DateTime.MinValue.Ticks || ticks > DateTime.MaxValue.Ticks) return default;
+        return new DateTime(ticks);
+    }
+
     /// <inheritdoc/>
-    public static DateTime ToDateTime(UInt64 value) => Convert.ToDateTime(value);
+    public static DateTime ToDateTime(ulong value)
+    {
+        // ulong 可能超出 long 范围
+        if (value > (ulong)DateTime.MaxValue.Ticks) return default;
+        long ticks = (long)value;
+        if (ticks < DateTime.MinValue.Ticks || ticks > DateTime.MaxValue.Ticks) return default;
+        return new DateTime(ticks);
+    }
+
     /// <inheritdoc/>
-    public static DateTime ToDateTime(Single value) => Convert.ToDateTime(value);
+    public static DateTime ToDateTime(float value)
+    {
+        // System.Convert.ToDateTime(float) 先转 double，再转 ticks
+        if (float.IsNaN(value) || float.IsInfinity(value)) return default;
+        double d = value;
+        if (d < DateTime.MinValue.Ticks || d > DateTime.MaxValue.Ticks) return default;
+        return new DateTime((long)d);
+    }
+
     /// <inheritdoc/>
-    public static DateTime ToDateTime(Double value) => Convert.ToDateTime(value);
+    public static DateTime ToDateTime(double value)
+    {
+        if (double.IsNaN(value) || double.IsInfinity(value)) return default;
+        if (value < DateTime.MinValue.Ticks || value > DateTime.MaxValue.Ticks) return default;
+        return new DateTime((long)value);
+    }
+
     /// <inheritdoc/>
-    public static DateTime ToDateTime(Decimal value) => Convert.ToDateTime(value);
+    public static DateTime ToDateTime(decimal value)
+    {
+        if (value < DateTime.MinValue.Ticks || value > DateTime.MaxValue.Ticks) return default;
+        return new DateTime((long)value);
+    }
+
     /// <inheritdoc/>
     public static DateTime ToDateTime(DateTime value) => value;
 }
