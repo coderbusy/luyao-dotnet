05792199fd409a4c98c38ba21a2ddb8d3538395c
Soar360 <Soar360@live.com>
2025-07-28 12:02:55 +0800

    简化 RecordColumn 的 Set 方法并添加测试
    在 `RecordColumn.Data.cs` 文件中，多个 `Set` 方法的实现进行了简化，移除了重复的代码。现在每个 `Set` 方法在设置值之前都会检查 `_table` 的行数，如果行数为零，则自动添加一行，以确保表中至少有一行可用。
    
    在 `RecordTests.cs` 文件中，新增了一系列测试方法，以验证在表为空时的边界条件，包括索引为零时的自动行创建、负索引的异常抛出以及大于零索引的异常处理。

--- src/LuYao.Common/Data/RecordColumn.Data.cs
+++ src/LuYao.Common/Data/RecordColumn.Data.cs
@@ -11,135 +11,135 @@ partial class RecordColumn
     /// <inheritdoc/>
     public void Set(Boolean value, int index)
     {
+        if (_table.Count == 0) _table.AddRow();
         if (index < 0 || index >= _table.Count)
             throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
-        if (_table.Count == 0) _table.AddRow();
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Byte value, int index)
     {
+        if (_table.Count == 0) _table.AddRow();
         if (index < 0 || index >= _table.Count)
             throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
-        if (_table.Count == 0) _table.AddRow();
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Char value, int index)
     {
+        if (_table.Count == 0) _table.AddRow();
         if (index < 0 || index >= _table.Count)
             throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
-        if (_table.Count == 0) _table.AddRow();
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(DateTime value, int index)
     {
+        if (_table.Count == 0) _table.AddRow();
         if (index < 0 || index >= _table.Count)
             throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
-        if (_table.Count == 0) _table.AddRow();
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Decimal value, int index)
     {
+        if (_table.Count == 0) _table.AddRow();
         if (index < 0 || index >= _table.Count)
             throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
-        if (_table.Count == 0) _table.AddRow();
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Double value, int index)
     {
+        if (_table.Count == 0) _table.AddRow();
         if (index < 0 || index >= _table.Count)
             throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
-        if (_table.Count == 0) _table.AddRow();
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Int16 value, int index)
     {
+        if (_table.Count == 0) _table.AddRow();
         if (index < 0 || index >= _table.Count)
             throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
-        if (_table.Count == 0) _table.AddRow();
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Int32 value, int index)
     {
+        if (_table.Count == 0) _table.AddRow();
         if (index < 0 || index >= _table.Count)
             throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
-        if (_table.Count == 0) _table.AddRow();
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Int64 value, int index)
     {
+        if (_table.Count == 0) _table.AddRow();
         if (index < 0 || index >= _table.Count)
             throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
-        if (_table.Count == 0) _table.AddRow();
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(SByte value, int index)
     {
+        if (_table.Count == 0) _table.AddRow();
         if (index < 0 || index >= _table.Count)
             throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
-        if (_table.Count == 0) _table.AddRow();
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Single value, int index)
     {
+        if (_table.Count == 0) _table.AddRow();
         if (index < 0 || index >= _table.Count)
             throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
-        if (_table.Count == 0) _table.AddRow();
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(String value, int index)
     {
+        if (_table.Count == 0) _table.AddRow();
         if (index < 0 || index >= _table.Count)
             throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
-        if (_table.Count == 0) _table.AddRow();
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(UInt16 value, int index)
     {
+        if (_table.Count == 0) _table.AddRow();
         if (index < 0 || index >= _table.Count)
             throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
-        if (_table.Count == 0) _table.AddRow();
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(UInt32 value, int index)
     {
+        if (_table.Count == 0) _table.AddRow();
         if (index < 0 || index >= _table.Count)
             throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
-        if (_table.Count == 0) _table.AddRow();
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(UInt64 value, int index)
     {
+        if (_table.Count == 0) _table.AddRow();
         if (index < 0 || index >= _table.Count)
             throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
-        if (_table.Count == 0) _table.AddRow();
         this.Data.Set(value, index);
     }
 
