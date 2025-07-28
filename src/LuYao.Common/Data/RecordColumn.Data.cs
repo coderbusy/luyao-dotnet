bf9b7a34da3a96e08261a8cc361b8c7019b49471
Soar360 <Soar360@live.com>
2025-07-28 13:20:17 +0800

    优化 RecordColumn 类的索引验证逻辑
    引入了 `ValidateIndex` 和 `ValidateReadIndex` 两个私有方法，简化了多个 `Set` 和 `To` 方法中的索引验证逻辑，减少了代码重复性，提高了可维护性。`Set` 方法现在支持在表为空时自动创建行，所有相关方法已更新为调用新的验证方法。

--- src/LuYao.Common/Data/RecordColumn.Data.cs
+++ src/LuYao.Common/Data/RecordColumn.Data.cs
@@ -8,138 +8,132 @@ namespace LuYao.Data;
 
 partial class RecordColumn
 {
+    /// <summary>
+    /// 验证索引是否在有效范围内，并在需要时自动创建行
+    /// </summary>
+    /// <param name="index">要验证的行索引</param>
+    /// <param name="autoCreateRow">是否在表为空时自动创建行</param>
+    /// <exception cref="ArgumentOutOfRangeException">当索引超出有效范围时抛出</exception>
+    private void ValidateIndex(int index, bool autoCreateRow = false)
+    {
+        if (autoCreateRow && _table.Count == 0) _table.AddRow();
+
+        if (index < 0 || index >= _table.Count) throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+    }
+
+    /// <summary>
+    /// 验证只读操作的索引是否在有效范围内
+    /// </summary>
+    /// <param name="index">要验证的行索引</param>
+    /// <exception cref="ArgumentOutOfRangeException">当索引超出有效范围时抛出</exception>
+    private void ValidateReadIndex(int index)
+    {
+        if (index < 0 || index >= _table.Count)
+            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+    }
+
     /// <inheritdoc/>
     public void Set(Boolean value, int index)
     {
-        if (_table.Count == 0) _table.AddRow();
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateIndex(index, autoCreateRow: true);
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Byte value, int index)
     {
-        if (_table.Count == 0) _table.AddRow();
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateIndex(index, autoCreateRow: true);
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Char value, int index)
     {
-        if (_table.Count == 0) _table.AddRow();
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateIndex(index, autoCreateRow: true);
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(DateTime value, int index)
     {
-        if (_table.Count == 0) _table.AddRow();
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateIndex(index, autoCreateRow: true);
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Decimal value, int index)
     {
-        if (_table.Count == 0) _table.AddRow();
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateIndex(index, autoCreateRow: true);
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Double value, int index)
     {
-        if (_table.Count == 0) _table.AddRow();
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateIndex(index, autoCreateRow: true);
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Int16 value, int index)
     {
-        if (_table.Count == 0) _table.AddRow();
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateIndex(index, autoCreateRow: true);
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Int32 value, int index)
     {
-        if (_table.Count == 0) _table.AddRow();
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateIndex(index, autoCreateRow: true);
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Int64 value, int index)
     {
-        if (_table.Count == 0) _table.AddRow();
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateIndex(index, autoCreateRow: true);
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(SByte value, int index)
     {
-        if (_table.Count == 0) _table.AddRow();
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateIndex(index, autoCreateRow: true);
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(Single value, int index)
     {
-        if (_table.Count == 0) _table.AddRow();
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateIndex(index, autoCreateRow: true);
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(String value, int index)
     {
-        if (_table.Count == 0) _table.AddRow();
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateIndex(index, autoCreateRow: true);
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(UInt16 value, int index)
     {
-        if (_table.Count == 0) _table.AddRow();
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateIndex(index, autoCreateRow: true);
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(UInt32 value, int index)
     {
-        if (_table.Count == 0) _table.AddRow();
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateIndex(index, autoCreateRow: true);
         this.Data.Set(value, index);
     }
 
     /// <inheritdoc/>
     public void Set(UInt64 value, int index)
     {
-        if (_table.Count == 0) _table.AddRow();
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateIndex(index, autoCreateRow: true);
         this.Data.Set(value, index);
     }
 
@@ -147,120 +141,105 @@ partial class RecordColumn
     /// <inheritdoc/>
     public Boolean ToBoolean(int index)
     {
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateReadIndex(index);
         return this.Data.ToBoolean(index);
     }
 
     /// <inheritdoc/>
     public Byte ToByte(int index)
     {
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateReadIndex(index);
         return this.Data.ToByte(index);
     }
 
     /// <inheritdoc/>
     public Char ToChar(int index)
     {
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateReadIndex(index);
         return this.Data.ToChar(index);
     }
 
     /// <inheritdoc/>
     public DateTime ToDateTime(int index)
     {
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateReadIndex(index);
         return this.Data.ToDateTime(index);
     }
 
     /// <inheritdoc/>
     public Decimal ToDecimal(int index)
     {
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateReadIndex(index);
         return this.Data.ToDecimal(index);
     }
 
     /// <inheritdoc/>
     public Double ToDouble(int index)
     {
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateReadIndex(index);
         return this.Data.ToDouble(index);
     }
 
     /// <inheritdoc/>
     public Int16 ToInt16(int index)
     {
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateReadIndex(index);
         return this.Data.ToInt16(index);
     }
 
     /// <inheritdoc/>
     public Int32 ToInt32(int index)
     {
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateReadIndex(index);
         return this.Data.ToInt32(index);
     }
 
     /// <inheritdoc/>
     public Int64 ToInt64(int index)
     {
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateReadIndex(index);
         return this.Data.ToInt64(index);
     }
 
     /// <inheritdoc/>
     public SByte ToSByte(int index)
     {
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateReadIndex(index);
         return this.Data.ToSByte(index);
     }
 
     /// <inheritdoc/>
     public Single ToSingle(int index)
     {
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateReadIndex(index);
         return this.Data.ToSingle(index);
     }
 
     /// <inheritdoc/>
     public String ToString(int index)
     {
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateReadIndex(index);
         return this.Data.ToString(index);
     }
 
     /// <inheritdoc/>
     public UInt16 ToUInt16(int index)
     {
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateReadIndex(index);
         return this.Data.ToUInt16(index);
     }
 
     /// <inheritdoc/>
     public UInt32 ToUInt32(int index)
     {
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateReadIndex(index);
         return this.Data.ToUInt32(index);
     }
 
     /// <inheritdoc/>
     public UInt64 ToUInt64(int index)
     {
-        if (index < 0 || index >= _table.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), $"行索引 {index} 超出有效范围 [0, {_table.Count - 1}]");
+        ValidateReadIndex(index);
         return this.Data.ToUInt64(index);
     }
 }
