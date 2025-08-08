558aacca2f1ed920f61a8027bd1d4bd7b6f62ae2
Soar360 <soar360@live.com>
2025-08-08 19:44:51 +0800

    添加调试类型代理以增强 Record 类的可视化
    在 `Record.cs` 中引入 `System.Diagnostics` 命名空间，并为 `Record` 类添加了 `[DebuggerTypeProxy(typeof(RecordDebuggerTypeProxy))]` 属性。
    新增 `RecordDebuggerTypeProxy` 内部类，提供 `Record` 类的调试信息，包括名称、计数和数据字符串表示。

--- src/LuYao.Common/Data/Record.cs
+++ src/LuYao.Common/Data/Record.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ namespace LuYao.Data;
 /// <summary>
 /// 列存储数据集合
 /// </summary>
+[DebuggerTypeProxy(typeof(RecordDebuggerTypeProxy))]
 public partial class Record : IEnumerable<RecordRow>, IRecordCursor
 {
     /// <summary>
---
+++ src/LuYao.Common/Data/RecordDebuggerTypeProxy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuYao.Data;
+
+internal class RecordDebuggerTypeProxy
+{
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private readonly Record _record;
+
+    public RecordDebuggerTypeProxy(Record record)
+    {
+        _record = record ?? throw new ArgumentNullException(nameof(record));
+    }
+    public string Name => string.IsNullOrWhiteSpace(_record.Name) ? "None" : _record.Name;
+    public int Count => _record.Count;
+    public string Data => _record.ToString();
+}
