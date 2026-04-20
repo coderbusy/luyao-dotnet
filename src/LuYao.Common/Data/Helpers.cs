e593355feab3d0a01b23c8b8bf9ef77857e12a85
Soar360 <soar360@live.com>
2026-04-20 15:02:19 +0800

    移除 RecordColumn 构造相关缓存与工厂方法
    删除了 Helpers.cs 中所有 RecordColumn 构造函数缓存、表达式树动态创建及 MakeRecordColumn 方法，仅保留 NormalizeColumnLookupType 方法，简化了 Helpers 类实现。

--- src/LuYao.Common/Data/Helpers.cs
+++ src/LuYao.Common/Data/Helpers.cs
@@ -1,8 +1,6 @@
 ﻿using LuYao.Data.Meta;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 
 namespace LuYao.Data;
 
@@ -113,54 +111,4 @@ static class Helpers
         var effective = Nullable.GetUnderlyingType(type) ?? type;
         return effective.IsEnum ? Enum.GetUnderlyingType(effective) : effective;
     }
-
-    private static readonly ConcurrentDictionary<Type, Func<Record, string, Type, RecordColumn>> _cache = new();
-
-    private static Func<Record, string, Type, RecordColumn> GetConstructor(Type type)
-    {
-        return _cache.GetOrAdd(type, static t =>
-        {
-            // 创建泛型类型
-            var genericType = typeof(RecordColumn<>).MakeGenericType(t);
-
-            // 获取构造函数
-            var ctor = genericType.GetConstructor(new[] {
-                typeof(Record),
-                typeof(string),
-                typeof(Type)
-            }) ?? throw new InvalidOperationException($"Constructor not found for {genericType}");
-
-            // 定义参数表达式
-            var pRecord = Expression.Parameter(typeof(Record), "record");
-            var pName = Expression.Parameter(typeof(string), "name");
-            var pType = Expression.Parameter(typeof(Type), "type");
-
-            // 创建构造函数调用表达式
-            var newExpr = Expression.New(ctor, pRecord, pName, pType);
-
-            // 编译表达式树为委托
-            return Expression.Lambda<Func<Record, string, Type, RecordColumn>>(
-                newExpr, pRecord, pName, pType).Compile();
-        });
-    }
-
-    public static RecordColumn MakeRecordColumn<T>(Record record, string name)
-    {
-        // 使用泛型方法优化，避免运行时查找类型
-        var ctor = GetConstructor(typeof(T));
-        return ctor.Invoke(record, name, typeof(T));
-    }
-
-    public static RecordColumn MakeRecordColumn(Record record, string name, Type type)
-    {
-        if (record == null) throw new ArgumentNullException(nameof(record));
-        if (name == null) throw new ArgumentNullException(nameof(name));
-        if (type == null) throw new ArgumentNullException(nameof(type));
-
-        ValidateColumnType(type);
-
-        var ctor = GetConstructor(type);
-        return ctor.Invoke(record, name, type);
-    }
-
 }
