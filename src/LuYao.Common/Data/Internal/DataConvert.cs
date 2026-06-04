33d7d86d9ecb8765699b80ce778e7621c25b81ab
Soar360 <Soar360@live.com>
2026-06-04 13:33:31 +0800

    支持枚举类型的类型转换
    在 ChangeType 方法中增加了对枚举类型的支持：当目标类型为枚举且输入为字符串时，使用 Enum.Parse 进行转换，否则使用 Enum.ToObject。提升了对枚举类型转换的兼容性。

--- src/LuYao.Common/Data/Internal/DataConvert.cs
+++ src/LuYao.Common/Data/Internal/DataConvert.cs
@@ -13,6 +13,17 @@ internal static class DataConvert
     public static object ChangeType(object value, Type type)
     {
         Type underlyingType = Nullable.GetUnderlyingType(type) ?? type;
+        if (underlyingType.IsEnum)
+        {
+            if (value is string str)
+            {
+                return Enum.Parse(underlyingType, str);
+            }
+            else
+            {
+                return Enum.ToObject(underlyingType, value);
+            }
+        }
         return Convert.ChangeType(value, underlyingType);
     }
 }
